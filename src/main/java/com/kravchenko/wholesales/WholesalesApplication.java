a7e0af142fc25bdcbd4fd6ea850933893cad43bc
Glaz0k <nikitakravcenko892@gmail.com>
2025-01-19 23:46:35 +0300

    Initial commit

---
+++ src/main/java/com/kravchenko/wholesales/WholesalesApplication.java
@@ -0,0 +1,13 @@
+package com.kravchenko.wholesales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WholesalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WholesalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kravchenko/wholesales/WholesalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kravchenko.wholesales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WholesalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
